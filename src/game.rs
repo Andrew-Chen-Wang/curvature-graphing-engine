eceb408be5d140baf4d7cd80a2bd345c606fc3f9
Andrew-Chen-Wang <acwangpython@gmail.com>
2020-07-04 15:29:05 -0400

    Added bottom menu with icons in position * Made the bool options correct like menu_shown and light_on * TODO Need to get the hide/show menu button in position

--- src/game.rs
+++ src/game.rs
@@ -1,13 +1,13 @@
 use amethyst::{
     assets::{AssetStorage, Handle, Loader},
     controls::HideCursor,
+    core::Hidden,
     ecs::Entity,
     input::{is_close_requested, is_key_down, is_mouse_button_down},
-    prelude::Trans,
-    prelude::{World, WorldExt},
+    prelude::{Builder, Trans, World, WorldExt},
     renderer::{ImageFormat, Texture},
-    ui::{Anchor, UiButtonBuilder, UiEventType, UiImage},
-    winit::{VirtualKeyCode, MouseButton},
+    ui::{Anchor, Interactable, UiButtonBuilder, UiEventType, UiImage, UiTransform},
+    winit::{MouseButton, VirtualKeyCode},
     GameData, SimpleState, SimpleTrans, StateData, StateEvent,
 };
 
@@ -21,17 +21,18 @@ pub struct GameState {
     paused: bool,
 
     // Buttons in the menu
-    show_hide_bottom_menu: u32,
+    background_image: Option<Entity>,
+    show_hide_bottom_menu: Option<Entity>,
     menu_shown: bool,
-    rotate_camera_button: u32,
+    rotate_camera_button: Option<Entity>,
     // Adds a sphere
-    add_object: u32,
+    add_object: Option<Entity>,
     // Morphing as in reshaping it
-    morph_object: u32,
+    morph_object: Option<Entity>,
     // Make it larger or smaller via the center
-    scale_object: u32,
-    delete_object: u32,
-    headlamp_button: u32,
+    scale_object: Option<Entity>,
+    delete_object: Option<Entity>,
+    headlamp_button: Option<Entity>,
 }
 
 // TODO In map.rs, we should add dir and coords
@@ -46,17 +47,10 @@ impl SimpleState for GameState {
         self.headlamp = Some(initialize_light(world));
 
         // Initialize menu
-        // Manually supply id in case of collision
-        self.rotate_camera_button = create_menu_button(world, "toolbar/3d_rotation.png", 100);
-        self.headlamp_button = create_menu_button(world, "toolbar/headlamp.png", 101);
-        self.show_hide_bottom_menu = create_menu_button(world, "toolbar/open_hide_menu.png", 102);
-        self.menu_shown = true;
-        self.add_object = create_menu_button(world, "toolbar/add_obj.png", 103);
-        self.morph_object = create_menu_button(world, "toolbar/wrench_edit.png", 104);
-        self.scale_object = create_menu_button(world, "toolbar/scale_obj.png", 105);
-        self.delete_object = create_menu_button(world, "toolbar/trash.png", 106);
+        self.create_menu(world);
 
-        // User can press the rotate button to rotate, so initially not in rotate.
+        // User can press the rotate button to rotate,
+        // so initially camera not in rotate mode.
         let mut hide_cursor = world.write_resource::<HideCursor>();
         hide_cursor.hide = false;
     }
@@ -97,30 +91,46 @@ impl SimpleState for GameState {
                 // TODO Allow point manipulation and central point scaling big/small.
             }
             StateEvent::Ui(ui_event) => {
-                let target_id = ui_event.target.id();
-                // TODO Add tooltips for menu buttons
-                if target_id == self.rotate_camera_button
-                    && ui_event.event_type == UiEventType::ClickStop
-                {
-                    // Press escape to get out of camera rotation.
-                    let mut hide_cursor = world.write_resource::<HideCursor>();
-                    hide_cursor.hide = true;
-                // Also to get out of an edit mode or the like
-                } else if target_id == self.headlamp_button {
-                    // Turns on and off the user's headlamp
-                    if self.light_on {
-                        if let Some(headlamp) = self.headlamp {
-                            world.delete_entity(headlamp);
+                let target_id = ui_event.target;
+                if ui_event.event_type == UiEventType::ClickStop {
+                    // TODO Add tooltips for menu buttons
+                    if target_id == self.rotate_camera_button.unwrap() {
+                        // Press escape to get out of camera rotation.
+                        let mut hide_cursor = world.write_resource::<HideCursor>();
+                        hide_cursor.hide = true;
+                    // Also to get out of an edit mode or the like
+                    } else if target_id == self.headlamp_button.unwrap() {
+                        // Turns on and off the user's headlamp
+                        if self.light_on {
+                            if let Some(headlamp) = self.headlamp {
+                                let _ = world.delete_entity(headlamp);
+                            }
+                            self.headlamp = None;
+                        } else {
+                            self.headlamp = Some(initialize_light(world));
                         }
-                        self.headlamp = None;
-                    } else {
-                        self.headlamp = Some(initialize_light(world));
-                    }
-                } else if target_id == self.show_hide_bottom_menu {
-                    // If menu hidden, show menu, and vice versa
-                    if self.menu_shown {
-
-                    } else {
+                        self.light_on = !self.light_on;
+                    } else if target_id == self.show_hide_bottom_menu.unwrap() {
+                        // If menu hidden, show menu, and vice versa
+                        if self.menu_shown {
+                            let mut hidden = world.write_storage::<Hidden>();
+                            let _ = hidden.insert(self.rotate_camera_button.unwrap(), Hidden);
+                            let _ = hidden.insert(self.add_object.unwrap(), Hidden);
+                            let _ = hidden.insert(self.morph_object.unwrap(), Hidden);
+                            let _ = hidden.insert(self.scale_object.unwrap(), Hidden);
+                            let _ = hidden.insert(self.delete_object.unwrap(), Hidden);
+                            let _ = hidden.insert(self.headlamp_button.unwrap(), Hidden);
+                            // show_hide_bottom_menu: Entity
+                        } else {
+                            let mut hidden = world.write_storage::<Hidden>();
+                            let _ = hidden.remove(self.rotate_camera_button.unwrap());
+                            let _ = hidden.remove(self.add_object.unwrap());
+                            let _ = hidden.remove(self.morph_object.unwrap());
+                            let _ = hidden.remove(self.scale_object.unwrap());
+                            let _ = hidden.remove(self.delete_object.unwrap());
+                            let _ = hidden.remove(self.headlamp_button.unwrap());
+                        }
+                        self.menu_shown = !self.menu_shown;
                     }
                 }
             }
@@ -130,19 +140,59 @@ impl SimpleState for GameState {
     }
 }
 
-fn create_menu_button(world: &mut World, path: &str, id: u32) -> u32 {
-    let texture_handle: Handle<Texture> = {
-        let loader = world.read_resource::<Loader>();
-        let texture_assets = world.read_resource::<AssetStorage<Texture>>();
-        loader.load(path, ImageFormat::default(), (), &texture_assets)
-    };
+impl GameState {
+    fn create_menu(&mut self, world: &mut World) {
+        self.menu_shown = true;
+        let menu_transform = UiTransform::new(
+            String::from("bottom_menu"),
+            Anchor::BottomMiddle,
+            Anchor::Middle,
+            0.,
+            0.,
+            0.,
+            700.,
+            300.,
+        );
+        let image = UiImage::SolidColor([0., 1., 0., 1.]);
+        let bi = world
+            .create_entity()
+            .with(menu_transform)
+            .with(image)
+            .with(Interactable)
+            .build();
+        self.background_image = Some(bi);
 
-    let (_button_id, button) = UiButtonBuilder::<(), u32>::default()
-        .with_id(id)
-        .with_position(0.0, -256.0)
-        .with_size(45., 45.)
-        .with_anchor(Anchor::TopMiddle)
-        .with_image(UiImage::Texture(texture_handle))
-        .build_from_world(&world);
-    return button.image_entity.id();
+        // Manually supply id in case of collision
+        self.rotate_camera_button =
+            self.create_menu_button(world, "toolbar/3d_rotation.png", -200., 100.);
+        self.headlamp_button = self.create_menu_button(world, "toolbar/headlamp.png", -100., 100.);
+        self.show_hide_bottom_menu =
+            self.create_menu_button(world, "toolbar/open_hide_menu.png", 200., 100.);
+        self.add_object = self.create_menu_button(world, "toolbar/add_obj.png", 50., 50.);
+        self.morph_object = self.create_menu_button(world, "toolbar/wrench_edit.png", 100., 50.);
+        self.delete_object = self.create_menu_button(world, "toolbar/trash.png", 50., 100.);
+        self.scale_object = self.create_menu_button(world, "toolbar/scale_obj.png", 100., 100.);
+    }
+
+    fn create_menu_button(
+        &mut self,
+        world: &mut World,
+        path: &str,
+        position_x: f32,
+        position_y: f32,
+    ) -> Option<Entity> {
+        let texture_handle: Handle<Texture> = {
+            let loader = world.read_resource::<Loader>();
+            let texture_assets = world.read_resource::<AssetStorage<Texture>>();
+            loader.load(path, ImageFormat::default(), (), &texture_assets)
+        };
+
+        let (_button_id, button) = UiButtonBuilder::<(), u32>::default()
+            .with_position(position_x, position_y)
+            .with_size(45., 45.)
+            .with_anchor(Anchor::BottomMiddle)
+            .with_image(UiImage::Texture(texture_handle))
+            .build_from_world(&world);
+        Some(button.image_entity)
+    }
 }
